782bc6e3407f1f89459f2e89afca13ae3458b1dd
harsh <narlon@163.com>
2018-09-12 15:10:08 +0800

    upload proj

---
+++ Assets/Scripts/DGToF/ForceResolution.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceResolution : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //指定游戏以640x960的分辨率打开游戏
+        Screen.SetResolution(640, 960, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DGToF/Login/ClickLogin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickLogin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnClick()
+    {
+        Application.LoadLevel("Town");
+    }
+}
