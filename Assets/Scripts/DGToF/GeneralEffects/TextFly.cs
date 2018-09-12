35344f5876d4bbf1ca77cbea4924090a25164b1b
harsh <narlon@163.com>
2018-09-12 20:12:13 +0800

    use spirit to replace ui/image

---
+++ Assets/Scripts/DGToF/GeneralEffects/TextFly.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFly : MonoBehaviour
+{
+    private Vector3 savedPos;
+    public float swaySpeed;
+    public float flyHeight;
+    // Use this for initialization
+    void Start ()
+    {
+        savedPos = this.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = savedPos + new Vector3(0, (float)System.Math.Sin(Time.time* swaySpeed) * flyHeight, 0);
+    }
+}
