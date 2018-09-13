74d34809c6a772d27c831f90a8fa21266a995b90
harsh <narlon@163.com>
2018-09-13 11:49:18 +0800

    add a bag form

---
+++ Assets/Scripts/DGToF/MainScene/BagItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BagItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnClick()
+    {
+       
+    }
+
+    public void UpdateImage(Sprite img)
+    {
+        var imgC = this.transform.GetComponentInChildren<Image>();
+        imgC.sprite = img;
+    }
+    public void UpdateText(string txt)
+    {
+        var textC = this.transform.GetComponentInChildren<Text>();
+        textC.text = txt;
+    }
+}
---
+++ Assets/Scripts/DGToF/MainScene/BagPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagPanel : MonoBehaviour {
+    public Transform[] grids;
+
+    public Transform GetEmptyGrid()
+    {
+
+        for (int i = 0; i < grids.Length; i++)
+        {
+
+            if (grids[i].childCount == 0)
+            {
+
+                return grids[i];
+
+            }
+
+        }
+
+        return null;
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
