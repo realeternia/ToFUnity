fa5805cc5d5bdc335b12a6e9d9d93b16ea77367c
harsh <narlon@163.com>
2018-09-18 14:17:10 +0800

    add config tool

---
+++ Script/compileCfgDll.bat
@@ -0,0 +1,20 @@
+cd ..\ConfigData
+
+IF NOT EXIST ".\ConfigData" MD ".\ConfigData"
+
+ExcelToCsv.exe
+cd .\ConfigData
+csc /target:library /r:../NarlonLib.dll /r:../BaseType.dll /out:../ConfigData.dll *.cs
+if not "%ERRORLEVEL%" == "0" GOTO :ERROR
+
+cd ..
+copy "./ConfigData.dll" "../Client/Assets/plugins/"
+@copy "./ConfigData.dll" "../Tool/bin/"
+
+GOTO :END
+
+:ERROR
+@ECHO COMPILATION FAILED
+PAUSE
+
+:END
