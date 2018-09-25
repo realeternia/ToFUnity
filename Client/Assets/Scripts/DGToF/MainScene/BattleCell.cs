94b3e99cb02a428169612be1ae16b61d48bfc378
harsh <narlon@163.com>
2018-09-25 15:04:22 +0800

    BattleCell can click now

---
+++ Client/Assets/Scripts/DGToF/MainScene/BattleCell.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCell : MonoBehaviour {
+    private SpriteRenderer render;
+    // Use this for initialization
+    void Start () {
+        render = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseUp()
+    {
+        render.color = Color.red;
+        Debug.Log("btn clicked!!!");
+    }
+}
