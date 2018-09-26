80d729cbfc2ae374d8454c6b5b0cf0302f6387b8
harsh <narlon@163.com>
2018-09-26 15:03:38 +0800

    add a str tag

--- Client/Assets/Scripts/DGToF/Control/PicLoader.cs
+++ Client/Assets/Scripts/DGToF/Control/PicLoader.cs
@@ -9,6 +9,8 @@ public class PicLoader
     private static PicLoader instance;
     public static PicLoader Instance { get { return instance ?? (instance = new PicLoader()); } }
 
+    private Dictionary<string, Sprite> imageCacheDict = new Dictionary<string, Sprite>();
+
     public IEnumerator Load(Image imgC, string path)
     {
         double startTime = (double) Time.time;
@@ -39,10 +41,12 @@ public class PicLoader
 
     public IEnumerator Load(SpriteRenderer sprR, string path)
     {
-        double startTime = (double)Time.time;
-
+        if (imageCacheDict.ContainsKey(path))
+        {
+            sprR.sprite = imageCacheDict[path];
+            yield break;
+        }
         var fileAddress = System.IO.Path.Combine(Application.streamingAssetsPath, path);
-        Debug.Log(fileAddress);
         //请求WWW
         WWW www = new WWW("file:///" + fileAddress);
 
@@ -50,7 +54,7 @@ public class PicLoader
 
         if (!string.IsNullOrEmpty(www.error))
         {
-            Debug.Log("WWW加载错误:" + www.error);
+            Debug.LogWarning("WWW load error : " + www.error);
             yield return null;
         }
         Texture2D texture = www.texture;
@@ -59,9 +63,9 @@ public class PicLoader
         Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
             new Vector2(0.5f, 0.5f));
         sprR.sprite = sprite;
+        imageCacheDict[path] = sprite;
 
-        startTime = (double)Time.time - startTime;
-        Debug.Log("WWW加载用时:" + startTime);
+        Debug.Log("WWW load finish : " + fileAddress);
         yield return null;
     }
 }
--- Client/Assets/Scripts/DGToF/MainScene/BattleCell.cs
+++ Client/Assets/Scripts/DGToF/MainScene/BattleCell.cs
@@ -4,13 +4,19 @@ using ConfigDatas;
 using UnityEngine;
 
 public class BattleCell : MonoBehaviour {
+
     private SpriteRenderer render;
     private SpriteRenderer cardImg;
-    public int Id;
+    private TextMesh hpText;
+    private TextMesh strText;
+    public int Id; //cellId
+    public int MonsterId;
     // Use this for initialization
     void Start () {
         render = GetComponent<SpriteRenderer>();
         cardImg = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        hpText = transform.Find("Blood").GetChild(0).GetComponent<TextMesh>();
+        strText = transform.Find("Str").GetChild(0).GetComponent<TextMesh>();
     }
 	
 	// Update is called once per frame
@@ -27,9 +33,12 @@ public class BattleCell : MonoBehaviour {
         }
 
         cell.IsHide = false;
+
         transform.Find("Blood").gameObject.SetActive(true);
-        
-        MonsterConfig monsterConfig = ConfigData.GetMonsterConfig(cell.MonsterId);
+        transform.Find("Str").gameObject.SetActive(true);
+
+        MonsterId = cell.MonsterId;
+        MonsterConfig monsterConfig = ConfigData.GetMonsterConfig(MonsterId);
         //Debug.Log("aaaa" + Id + "aaa" + cell.MonsterId + monsterConfig.Name);
         StartCoroutine(PicLoader.Instance.Load(cardImg, string.Format("Image/Monsters/{0}.jpg", monsterConfig.Url)));
 
@@ -46,5 +55,18 @@ public class BattleCell : MonoBehaviour {
             render.color = Color.red;
         else
             render.color = Color.blue;
+
+        MonsterConfig monsterConfig = ConfigData.GetMonsterConfig(MonsterId);
+        UpdateStr(monsterConfig.Atk);
+        UpdateHp(monsterConfig.Hp);
+    }
+
+    public void UpdateStr(int str)
+    {
+        strText.text = str.ToString();
+    }
+    public void UpdateHp(int hp)
+    {
+        hpText.text = hp.ToString();
     }
 }
